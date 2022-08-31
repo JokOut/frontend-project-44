0cc1a27a124ff4419554f213870b747049eae528
Leonid Kurevin <jokout321@gmail.com>
2022-08-31 21:29:09 +0300

    create game brain-even

---
+++ src/games-logic/brain-even-logic.js
@@ -0,0 +1,17 @@
+import playGame from '../games-body.js';
+
+import getRandom from '../tools.js';
+
+const isEven = (number) => number % 2 === 0;
+
+const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const generateRound = () => {
+  const value = getRandom();
+  const expectedAnswer = isEven(value) ? 'yes' : 'no';
+  return [expectedAnswer, value];
+};
+
+const runBrainEven = () => playGame(gameRule, generateRound);
+
+export default runBrainEven;
